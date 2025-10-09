eff7a674d71ee773f4476955474c2e245580eba5
timwlee <timothyl@adobe.com>
2025-10-09 12:47:59 -0700

    Update promotion block to take in new semantic html structure

--- blocks/promotion/promotion.js
+++ blocks/promotion/promotion.js
@@ -1,5 +1,24 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  block.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '1440' }])));
+  const elements = [...block.children];
+
+  const roleMap = {
+    0: 'desktop-image',
+    1: 'mobile-image',
+    2: 'rich-text-content',
+    3: 'legal-copy-text',
+  };
+
+  elements.forEach((child, index) => {
+    // Assign role (desktop/mobile) based on order
+    const role = roleMap[index];
+    if (role) child.classList.add(role);
+
+    // Process images for optimization
+    const img = child.querySelector('picture > img');
+    if (!img) return;
+    const optimizedPicture = createOptimizedPicture(img.src, img.alt, false, [{ width: '1440' }]);
+    img.closest('picture').replaceWith(optimizedPicture);
+  });
 }
